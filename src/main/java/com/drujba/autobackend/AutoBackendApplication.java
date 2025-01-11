c3ec755a9e41405bd9f3a2d8f69fc8dc1062b646
Estoutic <ilya_udalov_04@mail.ru>
2025-01-11 17:22:39 +0300

    __init__

---
+++ src/main/java/com/drujba/autobackend/AutoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.drujba.autobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoBackendApplication.class, args);
+    }
+
+}
